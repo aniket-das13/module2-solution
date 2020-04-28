acc9ce4552fd0de1100e1716e9b67a0f727b84a1
Aniket Das <58596896+aniket-das13@users.noreply.github.com>
2020-04-28 22:46:00 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var namelist=["Aniket","Anushka", "Junaid","JLo","Siddhisa", "Gopal","Justin"];
+
+	for(var x=0;x<namelist.length;x++){
+if((namelist[x])[0]=="J" || (namelist[x])[0]=="j"){
+
+	console.log("Goodbye "+ namelist[x]);
+
+}
+
+else console.log("Hello "+ namelist[x]);
+
+
+
+};
