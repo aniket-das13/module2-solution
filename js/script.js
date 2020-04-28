ffb55e14a91abfd5719685d5a113aa9b4d1ea177
Aniket Das <58596896+aniket-das13@users.noreply.github.com>
2020-04-28 23:02:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var namelist=["Aniket","Anushka", "Junaid","JLo","Siddhisa", "Gopal","Justin"];
+var namelist=["Yaakov","John", "Jason","Paul","Frank", "Larry","Paula","Laura","Jim"];
 
 	for(var x=0;x<namelist.length;x++){
 if((namelist[x])[0]=="J" || (namelist[x])[0]=="j"){
 
-	console.log("Goodbye "+ namelist[x]);
+	console.log("Good Bye "+ namelist[x]);
 
 }
 
@@ -12,3 +12,4 @@ else console.log("Hello "+ namelist[x]);
 
 
 };
+
